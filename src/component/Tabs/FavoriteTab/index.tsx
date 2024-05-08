62c38ac06ab2c1aa1b7c29d9eb2f99b84af35021
kirangautam45 <kirangautam225@gmail.com>
2024-05-08 22:32:18 +0545

    React assiment

--- src/component/Tabs/FavoriteTab/index.tsx
+++ src/component/Tabs/FavoriteTab/index.tsx
@@ -2,12 +2,24 @@ import { useAppContext } from '../../../context/SpellContext';
 import Card from '../../Card';
 
 const FavoriteTab = () => {
-  const { favoriteSpells } = useAppContext();
+  const { favoriteSpells, handleUrl, handleDialog } = useAppContext();
+
+  const handleClick = (url: string) => {
+    handleDialog();
+
+    handleUrl(url);
+  };
 
   return (
     <>
       {favoriteSpells.map((data) => (
-        <div key={data.index}>
+        <div
+          key={data.index}
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            handleClick(data.url);
+          }}
+        >
           <Card spell={data.name} />
         </div>
       ))}
